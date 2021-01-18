843dbd9d1f52bac8e7dbd6ddf8375e4290260435
nekvinder <nekvinder@gmail.com>
2021-01-18 18:57:19 +0530

    cleanup

--- api/urls.py
+++ api/urls.py
@@ -11,6 +11,5 @@ router.register(r'branches', BranchesViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
