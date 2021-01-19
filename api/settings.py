57ffecfa57feb00446d2bd32c23a0552e023c8da
nekvinder <nekvinder@gmail.com>
2021-01-20 03:35:47 +0530

    fix

--- api/settings.py
+++ api/settings.py
@@ -130,3 +130,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,10 @@ from rest_framework import routers
 from django.urls import include, path
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'banks', BanksViewSet)
@@ -12,4 +16,7 @@ router.register(r'branches', BranchesViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-]
+    url(r'^favicon\.ico$', RedirectView.as_view(
+        url='/static/favicon.ico')),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
